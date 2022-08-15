abd58c05cefc247158202e9fb9ff3b7e4aa5adb1
Aryan <aryan16072001236809@gmail.com>
2022-08-16 01:04:10 +0530

    posted to Database to cloud

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(express.static("public"))
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true})
+mongoose.connect("mongodb+srv://admin-Aryan:Test123@cluster0.qxpxiie.mongodb.net/todolistDB",{useNewUrlParser:true})
 
 const itemsSchema={
     name:String
